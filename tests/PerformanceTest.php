8cc4284fbe83bb463e9352dc8cd79436d8aa4ee9
Craig Duncan <craig@metroplexsystems.co.uk>
2019-03-08 15:45:53 +0000

    Update PHPUnit to the latest version

--- tests/PerformanceTest.php
+++ tests/PerformanceTest.php
@@ -7,13 +7,14 @@ use function extension_loaded;
 use function file_exists;
 use function file_get_contents;
 use function file_put_contents;
+use PHPUnit\Framework\TestCase;
 use function version_compare;
 
-class PerformanceTest extends \PHPUnit_Framework_TestCase
+class PerformanceTest extends TestCase
 {
     private $tmp = __DIR__ . "/data/tmp.edi";
 
-    public function setUp()
+    public function setUp(): void
     {
         if (extension_loaded("xdebug")) {
             $this->markTestSkipped("Cannot test performance as xdebug makes things slow");
@@ -32,7 +33,7 @@ class PerformanceTest extends \PHPUnit_Framework_TestCase
     }
 
 
-    public function tearDown()
+    public function tearDown(): void
     {
         if (file_exists($this->tmp)) {
             unlink($this->tmp);
